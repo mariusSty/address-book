5575136124e58dbbc79e83661b258b2eb9a913b8
Marius STEPHANY <marius.stephany@gmail.com>
2023-08-31 16:52:03 +0200

    :sparkles: Add custom font

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
+import React, { useCallback } from 'react';
 import { View, Text } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider onLayout={onLayoutRootView}>
       <View>
-        <Text>Hello</Text>
+        <Text style={{ fontFamily: 'Inter-Black', fontSize: 30 }}>Hello</Text>
       </View>
     </SafeAreaProvider>
   );
